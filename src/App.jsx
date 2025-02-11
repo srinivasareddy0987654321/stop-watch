68dd0f4c2aa5b76a64830ff45c7a465b2d371060
srinivasareddy0987654321 <srinivasareddy0987654321@gmail.com>
2025-02-11 22:27:41 +0530

    changes in timing function

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <div>
         <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time/10)%100))}</span>
+        <span>{("0" + ((time/10)%100)).slice(-2)}</span>
 
       </div>
       <div>
